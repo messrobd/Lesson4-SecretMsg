8bcc8f54fad9dab533ab1d3b549723ba318439dd
Rob <robmessenger@iMac.lan>
2018-03-14 16:15:58 +0100

    convert to functions

--- create_secret_message.py
+++ create_secret_message.py
@@ -5,18 +5,46 @@ program_wd = "/Users/messrobd 1/github/Lesson4-SecretMsg"
 
 os.chdir(program_wd)
 
-character_image_files = os.listdir("alphabet")
+def getCharacterImages(image_directory):
+    character_image_files = os.listdir("alphabet")
 
-i = 0
-for image in character_image_files:
-    if image[-4:] != ".jpg":
-        del character_image_files[i]
-    i += 1
+    image_extension, image_extension_length = ".jpg", 4
 
-print character_image_files
+    for index, image in enumerate(character_image_files):
+        if image[-image_extension_length:] != image_extension:
+            del character_image_files[index]
 
-alphabet = "abcdefghijklmnopqrstuvwxyz. "
-alphabet_images = {}
+    return character_image_files
 
-for index, character in enumerate(alphabet):
-    alphabet_images[character] = character_image_files[index]
+def makeAlphabetImageDict(character_image_files):
+    alphabet = "abcdefghijklmnopqrstuvwxyz. "
+    alphabet_images = {}
+
+    for index, character in enumerate(alphabet):
+        alphabet_images[character] = character_image_files[index]
+
+    return alphabet_images
+
+def makeMessageImageMapping(message, alphabet_images):
+    message_character_images = []
+    message_image_names = []
+
+    for index, character in enumerate(message):
+        message_character_images.append(alphabet_images[character])
+        message_image_names.append(character_image_files[index])
+
+    return message_character_images, message_image_names
+
+def makeMessageImages(image_directory, message_image_mapping):
+    message_character_images, message_image_names = message_image_mapping
+    target_directory = os.getcwd()
+
+    for source_name, target_name in zip(message_character_images, message_image_names):
+        source_name_path = "{0}/{1}".format(image_directory, source_name)
+        target_name_path = "{0}/{1}".format(target_directory, target_name)
+        os.rename(source_name_path, target_name_path)
+
+    return
+
+#execution
+message = "you smell"
